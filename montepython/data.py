c7aae5ba4b14ef74c32e376ab441ed674fecda75
jlvdb <jlvdb@astro.uni-bonn.de>
2020-10-15 10:23:05 +0200

    included K1000 requirements

--- montepython/data.py
+++ montepython/data.py
@@ -787,6 +787,36 @@ class Data(object):
         # translate any-one that is not directly a CLASS parameter into one.
         # The try: except: syntax ensures that the first call
         for elem in self.get_mcmc_parameters(['cosmo']):
+            # custom addition for KiDS-1000, see
+            # https://github.com/BStoelzner/KiDS-1000_MontePython_likelihood
+            if elem == 'S_8':
+                h = self.cosmo_arguments['h']
+                # infer sigma8 from S_8, h, omega_b, omega_cdm, and omega_nu (assuming one standard massive neutrino and omega_nu=m_nu/93.14)
+                omega_b = self.cosmo_arguments['omega_b']
+                omega_cdm = self.cosmo_arguments['omega_cdm']
+                #
+                try:
+                    omega_nu = self.cosmo_arguments['m_ncdm'] / 93.14
+                except:
+                    omega_nu = 0.
+                self.cosmo_arguments['sigma8'] = self.cosmo_arguments['S_8'] * math.sqrt((0.3*h**2) / (omega_b+omega_cdm+omega_nu))
+                del self.cosmo_arguments[elem]
+            # custom addition for KiDS-1000, see
+            # https://github.com/BStoelzner/KiDS-1000_MontePython_likelihood
+            if elem == 'c_min':
+                c_min = self.cosmo_arguments['c_min']
+                # if a_0 and a_1 are not defined use the standard values given in equation (30) in Mead et al. 2015
+                try:
+                    a_0 = self.cosmo_arguments['a_0']
+                except:
+                    a_0 = 1.03
+                try:
+                    a_1 = self.cosmo_arguments['a_1']
+                except:
+                    a_1 = -0.11
+                self.cosmo_arguments['eta_0'] = a_0 + a_1 * c_min
+                del self.cosmo_arguments['a_0']
+                del self.cosmo_arguments['a_1']
             # infer h from Omega_Lambda and delete Omega_Lambda
             if elem == 'Omega_Lambda':
                 omega_b = self.cosmo_arguments['omega_b']
